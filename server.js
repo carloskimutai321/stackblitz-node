122b24e1ebbab0ac57f21de164d7c048e6b1eeff
carloskimutai321 <127697311+carloskimutai321@users.noreply.github.com>
2023-03-18 15:47:53 -0400

    3 files modified, 1 added

--- server.js
+++ server.js
@@ -2,40 +2,51 @@ const http = require('http');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-  // console.log(req);
-  console.log(req.url);
+	// console.log(req);
+	console.log(req.url);
 
-  // set header content type
-  res.setHeader('Content-Type', 'text/html');
+	// set header content type
+	res.setHeader('Content-Type', 'text/html');
 
-  let path = './views/';
+	let path = './views/';
 
-  switch (req.url) {
-    case '/':
-      path += 'index.html';
-      break;
-	case '/about':
-		path += 'about';
-		break;
-	  default:
-		path += '404.html'
-		break;
-  }
+	switch (req.url) {
+		case '/':
+			path += 'index.html';
+			res.statusCode = 200;
+			break;
+		case '/about':
+			path += 'about-us.html';
+			res.statusCode = 200;
+			break;
+		case '/about-me':
+			res.statusCode = 301;
+			res.setHeader('Location', '/about');
+			res.end();
+			break;
+		default:
+			path += '404.html';
+			res.statusCode = 400;
+			break;
+	}
 
-  // send html file
-  fs.readFile(path, (err, data) => {
-    if (err) {
-      console.log(err);
-      res.end();
-    }
-    res.end(data);
-  });
+	// send html file
+	fs.readFile(path, (err, data) => {
+		if (err) {
+			// console.log(err);
 
-  // routing
+			res.end();
+		} else
+			// {res.end(data)};
+			res.write(data)
+		res.end();
+	});
+
+	// routing
 });
 
 // localhost is the default value for 2nd argument
-server.listen(5600, 'localhost', () => {
-  console.log('listening for requests on port 3000');
+server.listen(5500, /*'localhost'*/() => {
+	console.log('listening for requests on port 3000');
 });
 
